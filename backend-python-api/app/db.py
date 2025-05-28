0e7d6f67cc7ff18a3372abbe3f57142573329b4b
remember2hydrate <72785759+remember2hydrate@users.noreply.github.com>
2025-05-28 12:45:17 +0300

    cmon db

--- backend-python-api/app/db.py
+++ backend-python-api/app/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.utils.logger import logger
 
 DATABASE_URL = "postgresql+asyncpg://postgresql_bingepal_db_user:2WYQGIX7VEfka46ETQujHDuZtEVzSWJd@dpg-d0do94juibrs73d0e9m0-a.oregon-postgres.render.com/postgresql_bingepal_db"
 
