22cfb001155863265ca3e269fb98039a3b848acb
remember2hydrate <72785759+remember2hydrate@users.noreply.github.com>
2025-05-28 12:38:27 +0300

    correct db url =D

--- backend-python-api/app/db.py
+++ backend-python-api/app/db.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://postgresql://postgresql_bingepal_db_user:2WYQGIX7VEfka46ETQujHDuZtEVzSWJd@dpg-d0do94juibrs73d0e9m0-a.oregon-postgres.render.com/postgresql_bingepal_db"
+DATABASE_URL = "postgresql+asyncpg://postgresql_bingepal_db_user:2WYQGIX7VEfka46ETQujHDuZtEVzSWJd@dpg-d0do94juibrs73d0e9m0-a.oregon-postgres.render.com/postgresql_bingepal_db"
 
 try:
     engine = create_async_engine(DATABASE_URL, echo=False)
