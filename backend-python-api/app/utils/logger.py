9a03644b4517b9ac51690a2e2a0b11da84f4e8e8
Themis Charmpis <72785759+remember2hydrate@users.noreply.github.com>
2025-06-26 19:21:54 +0300

    2b1s

--- backend-python-api/app/utils/logger.py
+++ backend-python-api/app/utils/logger.py
@@ -16,10 +16,14 @@ formatter = logging.Formatter(
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 
-log_file_path = os.path.join(os.path.dirname(__file__), "../../logs/dev.log")
-os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
-file_handler = RotatingFileHandler(log_file_path, maxBytes=1_000_000, backupCount=3)
+log_dir = os.path.join(os.path.dirname(__file__), "..", "logs")
+os.makedirs(log_dir, exist_ok=True)
+log_path = os.path.join(log_dir, "dev.log")
+file_handler = RotatingFileHandler(log_path, maxBytes=1_000_000, backupCount=3)
 file_handler.setFormatter(formatter)
+
+if not os.path.exists(log_path):
+    open(log_path, "a").close()
 
 if not logger.hasHandlers():
     logger.addHandler(console_handler)
